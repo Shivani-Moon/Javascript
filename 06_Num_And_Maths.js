6d4066d740871998112e162e22a91bcbfb080be4
Shivani-Moon <shivani.it.2023@gmail.com>
2025-07-09 17:13:03 +0530

    Initial commit

---
+++ 06_Num_And_Maths.js
@@ -0,0 +1,13 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.constructor());
+console.log(balance.toExponential());
+console.log(balance.toFixed());
+console.log(balance.toLocaleString());
+console.log(balance.toPrecision());
+console.log(balance.toString());
+console.log(balance.valueOf());
