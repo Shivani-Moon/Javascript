5f5566f8368e8e429eb76f7436d2e77a6635d96f
Shivani-Moon <shivani.it.2023@gmail.com>
2025-07-17 09:58:41 +0530

    Functions*

--- 10_Functions.js
+++ 10_Functions.js
@@ -64,7 +64,20 @@ function handleObject(anyobject) {
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-handleObject(user)
+//handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
 
 
 
